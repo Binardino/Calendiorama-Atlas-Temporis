d8caadc73c43a6e94ee9dadf4d0874be40d81561
Binardino <langlois.robin@gmail.com>
2026-03-26 10:46:10 +0100

    feat(app): init flask-caching and flask-compress in create_app()

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from config import config
+from flask_caching import Cache
+from flask_compress import Compress
+
+cache    = Cache()
+compress = Compress()
 
 def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    
+    cache.init_app(app)
+    compress.init_app(app)
+
+    from api.borders import borders_bp
+    app.register_blueprint(borders_bp, url_prefix='/api')
+
     @app.route('/')
     def index():
         return render_template('index.html')

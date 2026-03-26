827d65895809720f7906d126e16bbc6446b384a2
Binardino <langlois.robin@gmail.com>
2026-03-26 15:04:05 +0100

    feat(calendars): add CalendarDate dataclass and CalendarConverter ABC

---
+++ calendars/base.py
@@ -0,0 +1,20 @@
+
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+@dataclass
+class CalendarDate:
+    year          : int
+    month         : int
+    day           : int
+    calendar_name : str
+    formatted     : str
+
+class CalendarConverter(ABC):
+    @abstractmethod
+    def from_jdn(self, jdn: int) -> CalendarDate:
+        pass
+
+    @abstractmethod
+    def to_jdn(self, date: CalendarDate) -> int:
+        raise NotImplementedError("to_jdn method not implemented for this calendar")

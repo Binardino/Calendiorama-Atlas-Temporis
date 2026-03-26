ed54a4518e6f406660ea7520d028bcc78f2cd340
Binardino <langlois.robin@gmail.com>
2026-03-26 16:11:21 +0100

    feat(calendars): implement JulianCalendar converter

---
+++ calendars/julian.py
@@ -0,0 +1,20 @@
+from convertdate import julian
+from calendars.base import CalendarConverter, CalendarDate
+
+class JulianCalendar(CalendarConverter):
+    def from_jdn(self, jdn:int) -> CalendarDate:
+        #Julian calendar
+        #julian lib returns a tuple (year, month, day)
+        year, month, day = julian.from_jd(jdn)
+
+        return CalendarDate(
+            year=year,
+            month=month,
+            day=day,
+            calendar_name="Julian",
+            formatted=f"{year:04d}-{month:02d}-{day:02d}"
+        )
+
+    def to_jdn(self, cal_date: CalendarDate) -> int:
+        # Implementation of Julian date to JDN conversion
+        return int(julian.to_jd(cal_date.year, cal_date.month, cal_date.day))

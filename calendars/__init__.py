d16b132ccf80360114a25cbca3e148f9315cfe87
Binardino <langlois.robin@gmail.com>
2026-04-02 18:47:06 +0200

    feat(calendars): export all 8 converters from __init__.py

--- calendars/__init__.py
+++ calendars/__init__.py
@@ -1,1 +1,8 @@
-
+from calendars.gregorian  import GregorianCalendar
+from calendars.julian     import JulianCalendar
+from calendars.coptic     import CopticCalendar
+from calendars.ethiopian  import EthiopianCalendar
+from calendars.hijri      import HijriCalendar
+from calendars.hebrew     import HebrewCalendar
+from calendars.persian    import PersianCalendar
+from calendars.japanese   import JapaneseCalendar

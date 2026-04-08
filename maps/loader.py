bc1a60af0d6e30cdc00ef08d90fb738d844bb81a
Binardino <langlois.robin@gmail.com>
2026-04-08 08:53:30 +0200

    feat(loader) : wip - add get_available_years. Filter all geojson names to list all available years

--- maps/loader.py
+++ maps/loader.py
@@ -3,6 +3,27 @@ import geopandas as gpd
 
 DATA_DIR = Path(__file__).parent.parent / "data" / "geojson"
 
+def get_available_years() -> list[int] :
+    historical_map_dir = DATA_DIR / "historical"
+    raw_map_list = [map_file for map_file in historical_map_dir.glob("*.geojson")]
+
+    year_list = []
+    for map_file in raw_map_list:
+        try:
+            name     = map_file.stem      # e.g. "map_1500"
+            year_str = name.split("_")[1] # e.g. "1500"
+            
+            if year_str.startswith("bc"):
+                year_str = year_str[2:]   # strip the _bc string e.g. "bc_500" → "500"
+                year     = -int(year_str) # e.g. "bc_500" → "-500"
+            else:
+                year = int(year_str)
+            year_list.append(year)
+        
+        except (IndexError, ValueError) as e:
+            print(f"Skipping file {map_file.name}: {e}")
+
+    return sorted(year_list)
 def load_geojson(path: Path) -> gpd.GeoDataFrame:
     full_path = DATA_DIR / path
     if not full_path.exists():
